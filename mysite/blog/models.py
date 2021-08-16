1611a78ee888814ec7c9d29744dd6540eab87528
vonred5 <82859955+vonred5@users.noreply.github.com>
2021-08-16 14:54:26 +0600

    new

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Actor(models.Model):
     name_actor = models.CharField(max_length=200)
     about_actor = models.TextField()
-    image_actor = models.ImageField(upload_to='blog/media', default="not image")
+    image_actor = models.ImageField(upload_to='actors', default="not image")
 
     def __str__(self):
         return self.name_actor
